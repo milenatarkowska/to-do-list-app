60fa3a8fd384f060e55f1087500f50c69fc7c424
milenatarkowska <milenatarkowska1@gmail.com>
2025-08-01 11:21:31 +0200

    Header created

--- App.vue
+++ App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <div class="header-container">
+      <Header />
+    </div>
 
   </div>
 </template>
+
+<script setup>
+import Header from "./components/header-comp.vue";
+</script>
+
---
+++ components/header-comp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar-comp">
+    <div class="right">
+      <!-- zmiana motywow jasny/ciemny -->
+    </div>
+    <div class="left">
+      <div class="card flex justify-center">
+        <Drawer v-model:visible="visible" header="Your to-do lists">
+          <Button label="Add new list" icon="pi pi-plus" />
+        </Drawer>
+        <Button icon="pi pi-arrow-right" @click="visible = true" />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import Button from 'primevue/button';
+
+import Drawer from 'primevue/drawer';
+
+import { ref } from "vue";
+
+const visible = ref(false);
+</script>
