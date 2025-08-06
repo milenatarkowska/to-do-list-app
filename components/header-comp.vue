a0a25bd3b6ad3e822965f5e145d91ac5290eec3c
milenatarkowska <milenatarkowska1@gmail.com>
2025-08-06 12:35:08 +0200

    Styled header-comp

--- components/header-comp.vue
+++ components/header-comp.vue
@@ -3,13 +3,15 @@
     <div class="left">
       <div class="card flex justify-center">
         <Drawer v-model:visible="visible" header="Your to-do lists">
-          <Button label="Add new list" icon="pi pi-plus" />
+          <hr>
+          <br>
+          <Button label="Add new list" icon="pi pi-plus" class="custom-colors"/>
         </Drawer>
-        <Button icon="pi pi-arrow-right" @click="visible = true" />
+        <Button icon="pi pi-arrow-right" class="custom-colors" @click="visible = true" />
       </div>
     </div>
     <div class="right">
-      <Button icon="pi pi-plus" aria-label="Add" />
+      <Button icon="pi pi-plus" class="custom-colors" aria-label="Add" />
     </div>
   </div>
 </template>
@@ -33,7 +35,18 @@ const visible = ref(false);
   flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
-  padding: 0.5rem;
+  padding: 1rem;
 }
-
+.custom-colors {
+  background-color: var(--accent) !important;
+  border: none;
+  color: #fbfbfe !important;
+  transition: all 0.3s ease-in-out;
+}
+.custom-colors:hover {
+    background-color: var(--accent) !important;
+    box-shadow: 0 0 10px #f4f2ef, 0 0 20px #f4f2ef;
+    transform: scale(1.02);
+    border: #f4f2ef !important;
+}
 </style>
