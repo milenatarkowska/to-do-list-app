07c38f6242ee8fd02b6f76469b7f55da321c67dc
milenatarkowska <milenatarkowska1@gmail.com>
2025-08-06 09:58:42 +0200

    Landing page created

---
+++ components/home-comp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="message">
+    <h1><b>{{t('landing')}}</b></h1>
+  </div>
+  <div class="button">
+    <Button size="large">{{ t('btnText') }}</Button>
+  </div>
+</template>
+
+<script setup>
+import Button from 'primevue/button';
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
--- views/HomeView.vue
+++ views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home-view">
-
+    <HomeComp />
   </div>
 </template>
+<script setup>
+import HomeComp from '@/components/home-comp.vue'
+</script>
