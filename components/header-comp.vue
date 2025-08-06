9a4e71fad63aada79144295dbb82f55355d12f3d
milenatarkowska <milenatarkowska1@gmail.com>
2025-08-06 09:58:25 +0200

    Changed name home->app

--- components/header-comp.vue
+++ components/header-comp.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="navbar-comp">
-    <div class="right">
-      <!-- zmiana motywow jasny/ciemny -->
-    </div>
+  <div class="header">
     <div class="left">
       <div class="card flex justify-center">
         <Drawer v-model:visible="visible" header="Your to-do lists">
@@ -10,6 +7,9 @@
         </Drawer>
         <Button icon="pi pi-arrow-right" @click="visible = true" />
       </div>
+    </div>
+    <div class="right">
+      <Button icon="pi pi-plus" aria-label="Add" />
     </div>
   </div>
 </template>
@@ -20,7 +20,20 @@ import Button from 'primevue/button';
 
 import Drawer from 'primevue/drawer';
 
+
 import { ref } from "vue";
 
 const visible = ref(false);
 </script>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem;
+}
+
+</style>
